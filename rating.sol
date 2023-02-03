a143e91fca1d75c34a04d7f03b7a5f33ecf224b8
jxmauricio <johnmauricio0308@gmail.com>
2023-02-03 13:30:00 -0800

    rating and hours basic

---
+++ rating.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Rating {
+    
+    struct customerRating{
+        address from;
+        uint rating;
+    }
+    mapping(address=>customerRating[]) public ratings;
+
+    function sendRatings(address customer,address waiter,uint rating) public {
+        ratings[waiter].push(customerRating(customer,rating));
+    }
+
+    function getRating(address waiter) public view returns (uint avgRating) {
+        uint sumRatings = 0;
+        for (uint i=0;i<ratings[waiter].length;i++){
+            sumRatings+=ratings[waiter][i].rating;
+        } 
+        uint avgRating = sumRatings/ratings[waiter].length;
+        return avgRating;
+
+    
+    }
+}
