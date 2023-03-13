f8a7638911eb86ca1b4d368e082db070a5254586
jxmauricio <johnmauricio0308@gmail.com>
2023-03-13 15:06:16 -0700

    Final version

--- dapp/ethereum/factory.js
+++ dapp/ethereum/factory.js
@@ -1,6 +1,6 @@
 import web3 from "./web3";
 import Factory from "./build/Factory.json";
-const addressOfDeployedFactory = '0xA650fa5DEE91C3010295250d0433C2a3EC74c998';
+const addressOfDeployedFactory = '0x2A87e3fF982eFed3BCDEF6D475CCf6908609fD40';
 //lets us interact with the factory contract and call its methods 
 const instance = new web3.eth.Contract(
   Factory.abi,
